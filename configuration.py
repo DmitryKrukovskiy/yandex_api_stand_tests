e91abf8dcbb4d3bcc584f9f5b86de7cc995d318e
Dmitry <dkrukovskiy@yandex.ru>
2023-02-19 23:18:56 +0400

    Third commit

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://0d1aabb3-ad13-4028-bcc7-908063967f6e.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://96bcdc8e-ca70-4fd3-9958-86fe688c3a6b.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 LOG_MAIN_PATH = "/api/logs/main/"
 USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
--- create_user_test.py
+++ create_user_test.py
@@ -1,100 +1,88 @@
 import sender_stand_request
 import data
 
-# эта функция меняет значения в параметре firstName
-def get_user_body(first_name):
-    # копирование словаря с телом запроса из файла data, чтобы не потерять данные в исходном словаре
-    current_body = data.user_body.copy()
-    # изменение значения в поле firstName
-    current_body["firstName"] = first_name
-    # возвращается новый словарь с нужным значением firstName
+response = sender_stand_request.post_new_user(data.user_body)
+auth_token = response.json()['authToken']
+
+
+def get_kit_body(name):
+    current_body = data.kit_body.copy()
+    current_body["name"] = name
     return current_body
 
-# Функция для позитивной проверки
-def positive_assert(first_name):
-    # В переменную user_body сохраняется обновленное тело запроса
-    user_body = get_user_body(first_name)
-    # В переменную user_response сохраняется результат запроса на создание пользователя:
-    user_response = sender_stand_request.post_new_user(user_body)
 
-    # Проверяется, что код ответа равен 201
-    assert user_response.status_code == 201
-    # Проверяется, что в ответе есть поле authToken, и оно не пустое
-    assert user_response.json()["authToken"] != ""
+def positive_assert(name):
+    kit_body = get_kit_body(name)
+    kit_response = sender_stand_request.post_new_client_kit(kit_body, auth_token)
 
-    # В переменную users_table_response сохраняется результат запроса на получение данных из таблицы user_model
-    users_table_response = sender_stand_request.get_users_table()
 
-    # Строка, которая должна быть в ответе
-    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
-               + user_body["address"] + ",,," + user_response.json()["authToken"]
+    assert kit_response.status_code == 201
 
-    # Проверка, что такой пользователь есть, и он единственный
-    assert users_table_response.text.count(str_user) == 1
+    assert kit_response.json()["name"] == name
 
-# Тест 1. Успешное создание пользователя
-# Параметр fisrtName состоит из 2 символов
-def test_create_user_2_letter_in_first_name_get_success_response():
-    positive_assert("Aa")
 
-def test_create_user_15_letter_in_first_name_get_success_response():
-    positive_assert("Ааааааааааааааа")
+def test_create_kit_1_letter_in_name_get_success_response():
+    positive_assert("g")
 
-def negative_assert_symbol(first_name):
-    user_body = get_user_body(first_name)
-    response = sender_stand_request.post_new_user(user_body)
-    assert response.status_code == 400
-    assert response.json()["code"] == 400
-    assert response.json()["message"] == "Имя пользователя введено некорректно. \
-Имя может содержать только русские или латинские буквы, длина должна быть не менее 2 и не более 15 символов"
 
-def test_create_user_1_letter_in_first_name_get_success_response():
-    negative_assert_symbol("А")
+def test_create_kit_511_letters_in_name_get_success_response():
+    positive_assert("Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd\
+abcdabcdabcdabcdabcdabcdabcdabcdabcdabAbcdabcdabcdabcdabcdabcda\
+bcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabAbcdabcd\
+abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc\
+dabcdabAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab\
+cdabcdabcdabcdabcdabAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd\
+abcdabcdabcdabcdabcdabcdabcdabAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd\
+abcdabcdabcdabcdabcdabcdabcdabcdabcdabAbcdabcdabcdabcdabc")
 
-def test_create_user_16_letter_in_first_name_get_success_response():
-    negative_assert_symbol("Аааааааааааааааa")
 
-def test_create_user_english_letter_in_first_name_get_success_response():
+def negative_assert_symbol(name):
+    kit_body = get_kit_body(name)
+    kit_response = sender_stand_request.post_new_client_kit(kit_body, auth_token)
+    assert kit_response.status_code == 400
+    assert kit_response.json()["code"] == 400
+
+
+def test_create_kit_empty_name_get_success_response():
+    negative_assert_symbol("")
+
+
+def test_create_kit_512_letters_in_name_get_success_response():
+    negative_assert_symbol("Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd\
+abcdabcdabcdabcdabcdabcdabcdabcdabcdabAbcdabcdabcdabcdabcdabcda\
+bcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabAbcdabcd\
+abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc\
+dabcdabAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab\
+cdabcdabcdabcdabcdabAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd\
+abcdabcdabcdabcdabcdabcdabcdabAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd\
+abcdabcdabcdabcdabcdabcdabcdabcdabcdabAbcdabcdabcdabcdabcd")
+
+
+def test_create_kit_english_letters_in_name_get_success_response():
     positive_assert("QWErty")
 
-def test_create_user_russian_letter_in_first_name_get_success_response():
+
+def test_create_kit_russian_letters_in_name_get_success_response():
     positive_assert("Мария")
 
-def test_create_user_has_space_in_first_name_get_error_response():
-    negative_assert_symbol("Человек и Ко")
 
-def test_create_user_has_special_symbol_in_first_name_get_error_response():
-    negative_assert_symbol("\"№%@\",")
+def test_create_kit_has_special_symbols_in_name_get_success_response():
+    positive_assert("\"№%@\",")
 
-def test_create_user_has_number_in_first_name_get_error_response():
-    negative_assert_symbol("123")
 
-def negative_assert_no_first_name(user_body):
-    response = sender_stand_request.post_new_user(user_body)
-    assert response.status_code == 400
-    assert response.json()["code"] == 400
-    assert response.json()["message"] == "Не все необходимые параметры были переданы"
+def test_create_kit_has_spaces_in_name_get_success_response():
+    positive_assert(" Человек и КО ")
 
-# Тест 10. Ошибка
-# В запросе нет параметра firstName
-def test_create_user_no_first_name_get_error_response():
-    # Копируется словарь с телом запроса из файла data в переменную user_body
-    # Иначе можно потерять данные из исходного словаря
-    user_body = data.user_body.copy()
-    # Удаление параметра firstName из запроса
-    user_body.pop("firstName")
-    # Проверка полученного ответа
-    negative_assert_no_first_name(user_body)
 
-# Тест 11. Ошибка
-# Параметр fisrtName состоит из пустой строки
-def test_create_user_empty_first_name_get_error_response():
-    # В переменную user_body сохраняется обновлённое тело запроса
-    user_body = get_user_body("")
-    # Проверка полученного ответа
-    negative_assert_no_first_name(user_body)
+def test_create_kit_has_numbers_in_name_get_success_response():
+    positive_assert("123")
 
-def test_create_user_number_type_first_name_get_error_response():
-    user_body = get_user_body(12)
-    response = sender_stand_request.post_new_user(user_body)
-    assert response.status_code == 400
+
+def test_create_kit_no_name_get_error_response():
+    kit_body = data.kit_body.copy()
+    kit_body.pop("name")
+    negative_assert_symbol(kit_body)
+
+
+def test_create_kit_intejer_in_name_get_success_response():
+    negative_assert_symbol(123)
